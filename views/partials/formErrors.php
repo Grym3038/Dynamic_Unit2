e44846958c1a933763bba93762b5ccd260979879
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-19 06:39:09 -0500

    Add form errors partial view

---
+++ views/partials/formErrors.php
@@ -0,0 +1,9 @@
+<?php if (!empty($errors) && count($errors) > 0) : ?>
+    <ul>
+        <?php foreach ($errors as $error) : ?>   
+        <li>
+            <?php echo $error; ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
