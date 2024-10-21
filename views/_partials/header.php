298562c5500c248699e491db14a9a122c13d75f6
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2024-10-21 13:01:39 -0500

    added style for artists page

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Song App</title>
     <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css" />
     <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./Css/Site.css" />
 </head>
-<body>
+<body class="container mt-4 bg-dark text-light rounded">
     <nav>
         <ul>
             <li>
