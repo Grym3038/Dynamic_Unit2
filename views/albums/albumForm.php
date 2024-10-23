58672ede2a169f0ef9c0a855dd3dc33d3db3efea
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2024-10-22 22:54:26 -0500

    Added styles for all forms

--- views/albums/albumForm.php
+++ views/albums/albumForm.php
@@ -10,21 +10,21 @@
 <h1>
     <?php echo ($album['id'] == 0 ? 'Add' : 'Edit') . ' Album'; ?>
 </h1>
-
-<form action="." method="post">
+<form action="." method="post" class="form-dark">
     <?php include('views/_partials/formErrors.php'); ?>
 
     <input type="hidden" name="action" value="editAlbum" />
     <input type="hidden" name="albumId" value="<?php echo $album['id']; ?>" />
 
-    <div>
+    <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" 
+        <input type="text" name="name" id="name" class="form-control" 
             value="<?php echo htmlspecialchars($album['name']); ?>" />
     </div>
-    <div>
+
+    <div >
         <label for="artistId">Artist</label>
-        <select name="artistId" id="artistId">
+        <select name="artistId" id="artistId" class="form-control">
             <?php foreach ($artists as $artist) : ?>
                 <option value="<?php echo $artist['id']; ?>"
                     <?php if ($album['artistId'] == $artist['id']) : ?>
@@ -35,13 +35,15 @@
             <?php endforeach; ?>
         </select>
     </div>
-    <div>
+
+    <div class="form-group">
         <label for="iPath">Image Path</label>
-        <input type="text" name="iPath" id="iPath" 
+        <input type="text" name="iPath" id="iPath" class="form-control"
             value="<?php echo htmlspecialchars($album['iPath']); ?>" />
     </div>
-    <div>
-        <input type="submit" value="Submit" />
+
+    <div class="form-group">
+        <input type="submit" value="Submit" class="btn btn-dark" />
 
         <?php
             if ($album['id'] == 0)
@@ -54,10 +56,9 @@
             }
         ?>
 
-        <a href="<?php echo $href; ?>">
-            Cancel
-        </a>
+        <a href="<?php echo $href; ?>" class="btn btn-cancel">Cancel</a>
     </div>
 </form>
+
 
 <?php include('views/_partials/footer.php'); ?>
