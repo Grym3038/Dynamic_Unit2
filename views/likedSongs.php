6f5f1e089a7c3c4722998e0a483d520dcea264e6
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-31 19:28:49 -0500

    Patch liked songs page

--- views/likedSongs.php
+++ views/likedSongs.php
@@ -24,52 +24,55 @@
             <input type="submit" value="Unlike All" class="btn btn-danger" />
         </form>
     </p>
-    <table class="table table-dark">
-        <thead>
-                <th>&nbsp;</th>
-                <th>Name</th>
-                <th>Duration</th>
-                <th>Artists</th>
-                <th>&nbsp;</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php  foreach($songs as $song) : ?>
+    <div class="table-responsive">
+        <table class="table table-dark">
+            <thead>
                 <tr>
-                    <td>
-                        <a href="?action=viewSong&songId=<?php echo $song['id']; ?>">
-                            <img src="<?php echo $song['imagePath']; ?>" class="album-thumbnail" />
-                        </a>
-                    </td>
-                    <td>
-                        <?php
-                            $href = '.?action=viewSong&songId=' . $song['id'];
-                        ?>
-                        <a href="<?php echo $href; ?>"
-                            class="link-light link-underline-opacity-0 link-underline-opacity-100-hover">
-                            <?php echo htmlspecialchars($song['name']); ?>
-                        </a>
-                    </td>
-                    <td>
-                        <?php echo formatTime($song['length']); ?>
-                    </td>
-                    <td>
-                        <?php artistList\build($song['artists']); ?>
-                    </td>
-                    <td>
-                        <form action="." method="post">
-                            <input type="hidden" name="action" value="toggleLiked" />
-                            <input type="hidden" name="songId" value="<?php echo $song['id']; ?>" />
-                            <input type="hidden" name="redirectTo" value="?action=listLikedSongs" />
-                            <button type="submit" class="btn btn-unlike pt-2">
-                                &nbsp;
-                            </button>
-                        </form>
-                    </td>
+                    <th>&nbsp;</th>
+                    <th>Name</th>
+                    <th>Duration</th>
+                    <th>Artists</th>
+                    <th>&nbsp;</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php  foreach($songs as $song) : ?>
+                    <tr>
+                        <td>
+                            <a href="?action=viewSong&songId=<?php echo $song['id']; ?>">
+                                <img src="<?php echo $song['imagePath']; ?>" class="album-thumbnail" />
+                            </a>
+                        </td>
+                        <td>
+                            <?php
+                                $href = '.?action=viewSong&songId=' . $song['id'];
+                            ?>
+                            <a href="<?php echo $href; ?>"
+                                class="link-light link-underline-opacity-0 link-underline-opacity-100-hover">
+                                <?php echo htmlspecialchars($song['name']); ?>
+                            </a>
+                        </td>
+                        <td>
+                            <?php echo formatTime($song['length']); ?>
+                        </td>
+                        <td>
+                            <?php artistList\build($song['artists']); ?>
+                        </td>
+                        <td>
+                            <form action="." method="post">
+                                <input type="hidden" name="action" value="toggleLiked" />
+                                <input type="hidden" name="songId" value="<?php echo $song['id']; ?>" />
+                                <input type="hidden" name="redirectTo" value="?action=listLikedSongs" />
+                                <button type="submit" class="btn btn-unlike pt-2">
+                                    &nbsp;
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 <?php endif; ?>
 
 <?php include('views/_partials/footer.php'); ?>
